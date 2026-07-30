6ca2a2a7146874b8e08e9db20522da8436fce79b
Fernanditho <ImClerami@gmail.com>
2026-07-30 08:48:25 +0700

    Add: Show Repository

---
+++ app/src/main/java/com/test/mazetv/repository/ShowRepository.kt
@@ -0,0 +1,12 @@
+package com.test.mazetv.repository
+
+import com.test.mazetv.data.api.ApiService
+import com.test.mazetv.data.models.Shows
+import javax.inject.Inject
+
+class ShowRepository @Inject constructor(private val apiService: ApiService) {
+
+  suspend fun getShows(): List<Shows> {
+    return apiService.getShows()
+  }
+}
