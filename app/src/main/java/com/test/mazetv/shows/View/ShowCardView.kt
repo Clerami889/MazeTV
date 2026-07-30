ff2b0155e07cb42735aa125b84408944ba1e2fa4
Fernanditho <ImClerami@gmail.com>
2026-07-30 09:32:27 +0700

    Update: ShowCardView UI

--- app/src/main/java/com/test/mazetv/shows/View/ShowCardView.kt
+++ app/src/main/java/com/test/mazetv/shows/View/ShowCardView.kt
@@ -8,25 +8,41 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.test.mazetv.R
 import com.test.mazetv.data.models.Shows
 
 @Composable
 fun ShowCardView(shows: Shows) {
   Card(
+      colors =
+          CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.secondaryContainer),
       modifier = Modifier.fillMaxSize().padding(vertical = 8.dp),
       elevation = CardDefaults.cardElevation(defaultElevation = 12.dp),
   ) {
     Row(modifier = Modifier.padding(16.dp)) {
-      Column() {
+      AsyncImage(
+          model = shows.image.medium,
+          contentDescription = stringResource(R.string.CardViewImageDescription),
+          modifier = Modifier,
+          contentScale = ContentScale.Crop,
+      )
+      Column(Modifier.padding(top = 24.dp, start = 12.dp)) {
         Text(text = shows.name, color = MaterialTheme.colorScheme.onSurface)
         Row() {
+          Icon(
+              painter = painterResource(R.drawable.baseline_star_24),
+              contentDescription = stringResource(R.string.CardViewIconDescription),
+          )
           Spacer(Modifier.width(4.dp))
           Text(text = shows.rating.average?.toString() ?: stringResource(R.string.RatingFallback))
         }
